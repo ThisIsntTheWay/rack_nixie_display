d62d607ac5b1b77687f21f3f2e1aa6bddc109652
The Man <ThisIsntTheWay@users.noreply.github.com>
2021-12-30 22:50:16 +0100

    Invoke webserver and tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <displayController.h>
+#include <websrv.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(15200);
+
+  webServerAPIs();
+  webServerStaticContent();
+
+  xTaskCreate(taskSetDisplay, "Display daemon", 6500, NULL, 1, NULL);
+  xTaskCreate(taskSetIndicators, "Indicator daemon", 6500, NULL, 1, NULL);
+  xTaskCreate(taskSetLeds, "LED daemon", 6500, NULL, 1, NULL);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
