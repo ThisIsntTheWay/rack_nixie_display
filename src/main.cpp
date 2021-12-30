27f237f95a13883c11fb6a3fbe41ddd37364bb10
The Man <ThisIsntTheWay@users.noreply.github.com>
2021-12-30 22:56:19 +0100

    Test code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,25 @@
 void setup() {
   Serial.begin(15200);
 
+  // ---------------------------------
+  // TEST CODE
+  const char *SSID = "Your SSID";
+  const char *WiFiPassword = "Your Password";
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, WiFiPassword);
+  Serial.print("Connecting to "); Serial.println(SSID);
+ 
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+ 
+  Serial.print(F("Connected. IP address is: "));
+  Serial.println(WiFi.localIP());
+  // ---------------------------------
+
   webServerAPIs();
   webServerStaticContent();
 
