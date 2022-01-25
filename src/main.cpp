683a51ec63587e116ae55942c505e0e7e88b61f8
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-01-25 14:18:46 +0100

    Add factory reset by button.
    For now, this logic resides in loop()

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,31 @@
 #include <LITTLEFS.h>
 #include <networkConfig.h>
 
+/* -----------------------
+            VARS
+   ----------------------- */
+
+#define INTERACT_BUTTON_PIN 21
+#define RESET_DELAY_MS 5000
+
+TaskHandle_t taskDisplay;
+TaskHandle_t taskIndicator;
+TaskHandle_t taskOLed;
+TaskHandle_t taskTLed;
+TaskHandle_t taskTime;
+
+bool buttonState = true;
+bool buttonIsBeingPressed = false;
+
+/* -----------------------
+            MAIN
+   ----------------------- */
+
 void setup() {
   Serial.begin(115200);
 
   DisplayController dController;
-  xTaskCreate(taskSetStatusLED, "O_LED daemon", 4000, NULL, 1, NULL);
+  xTaskCreate(taskSetStatusLED, "O_LED daemon", 4000, NULL, 4, &taskOLed);
   dController.OnboardLEDmode = 3;
 
   if (!LITTLEFS.begin()) {
@@ -23,14 +43,57 @@ void setup() {
   webServerInit();
   
   // Remaining tasks
-  xTaskCreate(taskSetDisplay, "Display daemon", 6500, NULL, 1, NULL);
-  xTaskCreate(taskSetIndicators, "Indicator daemon", 6500, NULL, 1, NULL);
-  xTaskCreate(taskSetLeds, "T_LED daemon", 4000, NULL, 1, NULL);
-  xTaskCreate(taskTimekeeper, "Time daemon", 4000, NULL, 1, NULL);
+  xTaskCreate(taskSetDisplay, "Display daemon", 6500, NULL, 5, &taskDisplay);
+  xTaskCreate(taskSetIndicators, "Indicator daemon", 6500, NULL, 4, &taskIndicator);
+  xTaskCreate(taskSetLeds, "T_LED daemon", 4000, NULL, 4, &taskTLed);
+  xTaskCreate(taskTimekeeper, "Time daemon", 4000, NULL, 3, &taskTime);
   dController.OnboardLEDmode = 0;
+
+  // Interact button
+  pinMode(INTERACT_BUTTON_PIN, INPUT_PULLUP);
 
   Serial.print(vt_green); Serial.println("System ready.");
   Serial.print(vt_default_colour);
 }
 
-void loop() {}
+void loop() {
+  // Factory reset watchdog
+  unsigned long currentMillis = millis();
+  static unsigned long previousMillis = 0L;
+
+  buttonState = digitalRead(INTERACT_BUTTON_PIN);
+  if (!buttonState) {
+    if (!buttonIsBeingPressed) {
+      buttonIsBeingPressed = true;
+
+      previousMillis = millis();
+    } else {
+      // Watch how long the button was pressed for.
+      if (currentMillis - previousMillis >= RESET_DELAY_MS) {
+        Serial.println(F("[!] System is being reset..."));
+        vTaskDelete(taskDisplay);
+        vTaskDelete(taskIndicator);
+        vTaskDelete(taskOLed);
+        vTaskDelete(taskTLed);
+        vTaskDelete(taskTime);
+
+        // Blank nixies, dim board LEDs
+        Nixies n; n.BlankDisplay();
+        ledcWrite(ONBOARD_LEDC_CHANNEL, 50);
+        ledcWrite(TUBE_LEDC_CHANNEL, 50);
+
+        LITTLEFS.end();
+        if (LITTLEFS.format()) {
+          Serial.println(F(" > Formatting complete."));
+        } else {
+          Serial.println(F(" > Could not format."));
+        }
+
+        Serial.println(F(" > Rebooting..."));
+        ESP.restart();
+      }
+    }
+  } else {
+    buttonIsBeingPressed = false;
+  }
+}
