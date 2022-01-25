632885ee70c7d1842038e47900a84269107db39a
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-01-25 20:46:40 +0100

    Update Clock.ps1
    Now uses the seperators as a seconds indicator.

--- test/PowerShell/display/Clock.ps1
+++ test/PowerShell/display/Clock.ps1
@@ -9,6 +9,9 @@ $uri = "http://$targetIP/api/display"
 while ($true) {
     [int]$time = Get-Date -f "HHmm"
 
+    # Invert indicator
+    $indicator = [bool]!((Invoke-RestMethod ($uri + "/indicators")).indicators.1)
+
     $b = @{
         "tubes" = @{
             "1" = @{ "val" = [math]::Truncate($time / 1000); "pwm" = 255 }
@@ -16,8 +19,12 @@ while ($true) {
             "3" = @{ "val" = [math]::Truncate(($time / 10) % 10); "pwm" = 255 }
             "4" = @{ "val" = [math]::Truncate($time % 10); "pwm" = 255 }
         }
-        "Indicators" = @{ "1" = $false }
-    } | convertto-json
+        "indicators" = @{
+            "1" = $false
+            "2" = $indicator
+        }
+    } | ConvertTo-Json
+
 
     Write-Host "[$(Get-Date -f 'HH:mm:ss')] " -NoNewline -fore cyan
     Invoke-RestMethod -Method POST -uri $uri -ContentType application/json -body $b | out-null
